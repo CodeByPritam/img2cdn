5df00c3ddcf09a474754f89cae8acee67a2391ce
CodeByPritam <hello@pritamadak.com>
2026-08-14 23:51:08 +0530

    added list all groups route

--- src/modules/groups/groups.route.ts
+++ src/modules/groups/groups.route.ts
@@ -3,10 +3,11 @@ import GroupsController from './groups.controller.js';
 
 // Create an instance of router
 const GroupsRouter = new Hono();
-GroupsRouter.post('/groups', GroupsController);
-GroupsRouter.get('/groups/:gkey', GroupsController);
-GroupsRouter.patch('/groups/:gkey', GroupsController);
-GroupsRouter.delete('/groups/:gkey', GroupsController);
+GroupsRouter.post('/groups', GroupsController); // Create Groups
+GroupsRouter.get('/groups', GroupsController); // List All Groups
+GroupsRouter.get('/groups/:gkey', GroupsController); // Get Specific Group
+GroupsRouter.patch('/groups/:gkey', GroupsController); // Edit Specific group
+GroupsRouter.delete('/groups/:gkey', GroupsController); // Delete Specific Group
 
 // Export
 export default GroupsRouter;
