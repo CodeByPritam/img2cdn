730384e291b47016cc4884c5b47ad3bc8fed92fd
CodeByPritam <hello@pritamadak.com>
2026-08-15 21:25:46 +0530

    refined groups logic again in route level

--- src/modules/groups/groups.route.ts
+++ src/modules/groups/groups.route.ts
@@ -4,10 +4,10 @@ import GroupsController from './groups.controller.js';
 // Create an instance of router
 const GroupsRouter = new Hono();
 GroupsRouter.post('/groups', GroupsController); // Create Groups
-GroupsRouter.get('/groups', GroupsController); // List All Groups
-GroupsRouter.get('/groups/:gkey', GroupsController); // Get Specific Group
-GroupsRouter.patch('/groups/:gkey', GroupsController); // Edit Specific group
-GroupsRouter.delete('/groups/:gkey', GroupsController); // Delete Specific Group
+GroupsRouter.get('/groups', GroupsController); // List All Groups From Both D1 & R2
+GroupsRouter.get('/groups/:gid', GroupsController); // Get Specific Group
+GroupsRouter.patch('/groups/:gid', GroupsController); // Edit Specific group
+GroupsRouter.delete('/groups/:gid', GroupsController); // Delete Specific Group
 
 // Export
 export default GroupsRouter;
